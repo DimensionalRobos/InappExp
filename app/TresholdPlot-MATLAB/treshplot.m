b622effb342ea6438b4a00fa163297befde79fa7
Joshua Dapitan <joshuadapitan@gmail.com>
2015-10-13 08:20:08 +0800

    Deploy MatLab Script
    Former commit does not include .m file

---
+++ app/TresholdPlot-MATLAB/treshplot.m
@@ -0,0 +1,9 @@
+function treshplot(X,Y,borderline)
+    plot(Y,X);
+    hold on;
+    border=1:length(Y);
+    for i=1:length(Y);
+        border(i)=borderline
+    end
+    plot(Y,border,'r');
+end
