98185174eb671be93ac0cc839038e4aecdafa8e3
Joshua Dapitan <joshuadapitan@gmail.com>
2015-11-26 00:45:02 +0800

    Configuration Revision
    Change path of udscrape.py

--- app/swntool-Java/src/Config.java
+++ app/swntool-Java/src/Config.java
@@ -9,7 +9,7 @@
  * @author Daikaiser
  */
 public class Config {
-    public static String PythonFolder="C:\\Users\\user\\Documents\\MATLAB\\";
+    public static String PythonFolder="C:\\Users\\root\\Documents\\GitHub\\InappExp\\app\\UrbanDictScraper-Python\\";
     public static String WordNetPath="C:\\Program Files\\WordNet\\2.1\\dict";
     public static String TrainingData="MLearn.txt";
     public static String NGramData="NGram.txt";
