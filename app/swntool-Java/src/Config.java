8e7588e14a8737b01dba0094dba6164f9fbcc20f
Joshua Dapitan <joshuadapitan@gmail.com>
2015-12-25 13:00:13 +0800

    SentiWordNetPath Deployed in Config
    *Cause of error is because the config variable SentiWordNetPath is
    Missing

--- app/swntool-Java/src/Config.java
+++ app/swntool-Java/src/Config.java
@@ -11,6 +11,7 @@
 public class Config {
     public static String PythonFolder="C:\\Users\\Anj Lasala\\Documents\\GitHub\\InappExp\\app\\UrbanDictScraper-Python\\";
     public static String WordNetPath="C:\\Program Files (x86)\\WordNet\\2.1\\dict";
+    public static String SentiWordNetPath="swn.txt";
     public static String TrainingData="MLearn.txt";
     public static String NGramData="NGram.txt";
     public static String BasisData="WordVal.txt";
